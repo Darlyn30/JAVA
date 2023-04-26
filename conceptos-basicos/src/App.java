a1451c841c13355bf93eb26dba760c4727c4642c
darlyn <zomberkawaiimusic@gmail.com>
2023-04-26 02:16:45 -0400

    proyecto de calculadora y conceptos basicos en java

---
+++ conceptos-basicos/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // estos son los conceptos basicos de java oracle
+        // llamar variables
+        var name = "hola, darlyn";
+        System.out.println(name);
+        // condicionales
+        // supongo que seran las mismas de todos los lenguajes, veamos
+        int n1 = 5;
+        int n2 = 9;
+
+        if(n1 > n2){
+            System.out.println(n1);
+        } else {
+            System.out.println(n2);
+        }
+        // ahora vamos a hacer algo un poco mas complejo
+        // ciclos
+        for(int i = 0; i < 10; i++){
+           System.out.println("hola mundo"); // esta linea es la salida, como el console.writeline de c# o el console.log de javascript 
+        }
+        // int num = 1;
+        // while(num > 0){
+        // } este while lo dejo vacio para que no se buguee
+        // las variables y los tipos de datos se llaman igual que en c# 
+
+        // hare un programa mas complejo que se encontrara en la carpeta projectoBasico
+    }
+}
---
+++ projectoBasico/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // aqui hare el programa clasico de la calculadora pero lo hare en una funcion
+        // si quiere calcular o salir
+
+        System.out.println("Bienvenido\n");
+        System.in.read();
+        System.out.flush(); // segun chat gpt esto funciona para limpiar la consola como un console clear,
+        // pero no reinicio, mas adelante sigo
+    }
+}
